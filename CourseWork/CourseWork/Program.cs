2bf4be094d5160635e9d92a983f0699854867eb1
alina <annadura@gmail.com>
2025-11-03 17:14:47 +0300

    Resolve merge

--- CourseWork/CourseWork/Program.cs
+++ CourseWork/CourseWork/Program.cs
@@ -1,3 +1,165 @@
+//using Microsoft.AspNetCore.HttpOverrides;
+//using CourseWork.Services;
+//using Microsoft.AspNetCore.Authentication.Cookies;
+//using Microsoft.AspNetCore.Authentication.Google;
+//using Microsoft.AspNetCore.Authentication.Facebook;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.AspNetCore.Identity;
+//using CourseWork.Models;
+//using Microsoft.Extensions.Options;
+//using CourseWork.Controllers;
+//using Microsoft.AspNetCore.Localization;
+//using Microsoft.AspNetCore.Mvc.Razor; 
+//using System.Globalization;
+//using Microsoft.AspNetCore.HttpOverrides;
+//var builder = WebApplication.CreateBuilder(args);
+
+
+//// ---------------- AUTH ----------------
+//builder.Services.AddAuthentication(options =>
+//{
+//    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+//    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+//})
+//.AddCookie()
+//.AddGoogle(googleOptions =>
+//{
+//    googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+//    googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+
+//    googleOptions.CallbackPath = "/signin-google";
+
+//    googleOptions.SaveTokens = true;
+//})
+//.AddFacebook(facebookOptions =>
+//{
+//    facebookOptions.AppId = builder.Configuration["Authentication:Facebook:AppId"];
+//    facebookOptions.AppSecret = builder.Configuration["Authentication:Facebook:AppSecret"];
+//    facebookOptions.CallbackPath = "/Registration/ExternalLoginCallback"; 
+//});
+
+
+//// ---------------- HTTPS ----------------
+//builder.Services.AddHttpsRedirection(options =>
+//{
+//    options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+//    options.HttpsPort = 443;
+//});
+
+//builder.Services.AddHsts(options =>
+//{
+//    options.Preload = true;
+//    options.IncludeSubDomains = true;
+//    options.MaxAge = TimeSpan.FromDays(60);
+//});
+
+//// ---------------- MVC + Localizer ----------------
+//builder.Services.AddControllersWithViews()
+//    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix) // 👈 добавлено
+//    .AddDataAnnotationsLocalization() // 👈 добавлено
+//    .AddSessionStateTempDataProvider();
+
+//// ---------------- EF + Identity ----------------
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseNpgsql(
+//        builder.Configuration.GetConnectionString("DefaultConnection"),
+//        npgsqlOptions =>
+//        {
+//            npgsqlOptions.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+//        })
+//        .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+//);
+
+//builder.Services.AddIdentity<User, IdentityRole<Guid>>(options =>
+//{
+//    options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+ ";
+//})
+//.AddEntityFrameworkStores<AppDbContext>()
+//.AddDefaultTokenProviders();
+
+//// ---------------- Session ----------------
+//builder.Services.AddDistributedMemoryCache();
+//builder.Services.AddSession(options =>
+//{
+//    options.IdleTimeout = TimeSpan.FromMinutes(30);
+//    options.Cookie.HttpOnly = true;
+//    options.Cookie.IsEssential = true;
+//    options.Cookie.SecurePolicy = builder.Environment.IsDevelopment()
+//        ? CookieSecurePolicy.SameAsRequest
+//        : CookieSecurePolicy.Always;
+//    options.Cookie.SameSite = SameSiteMode.Lax;
+//});
+
+//builder.Services.AddHttpContextAccessor();
+//builder.Services.AddSignalR();
+
+//// ---------------- Localization ----------------
+//builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+//builder.Services.Configure<RequestLocalizationOptions>(options =>
+//{
+//    var supportedCultures = new[] { "en-US", "ru-RU" };
+//    options.DefaultRequestCulture = new RequestCulture("en-US");
+//    options.SupportedCultures = supportedCultures.Select(c => new CultureInfo(c)).ToList();
+//    options.SupportedUICultures = supportedCultures.Select(c => new CultureInfo(c)).ToList();
+//    options.RequestCultureProviders = new[] { new CookieRequestCultureProvider() };
+//});
+
+//// ---------------- Build ----------------
+//var app = builder.Build();
+
+//// ---------------- Middleware ----------------
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    app.UseHsts();
+//}
+
+//app.UseForwardedHeaders(new ForwardedHeadersOptions
+//{
+//    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+//});
+
+//var fwdOptions = app.Services.GetRequiredService<IOptions<ForwardedHeadersOptions>>().Value;
+//fwdOptions.KnownNetworks.Clear();
+//fwdOptions.KnownProxies.Clear();
+
+//using (var scope = app.Services.CreateScope())
+//{
+//    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+//    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+//    var logger = scope.ServiceProvider.GetRequiredService<ILogger<RegistrationController>>();
+
+//    var registrationController = new RegistrationController(userManager, null, roleManager, logger);
+//    await registrationController.EnsureAdminExists();
+//}
+
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+
+//// 👇 вставь это ДО UseRouting()
+//var locOptions = app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value;
+//app.UseRequestLocalization(locOptions);
+
+//app.UseRouting();
+
+//app.UseForwardedHeaders(new ForwardedHeadersOptions
+//{
+//    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+//});
+
+//app.UseAuthentication();
+//app.UseAuthorization();
+//app.UseSession();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Registration}/{action=Register}/{id?}");
+
+//app.MapHub<CourseWork.Hubs.DiscussionHub>("/discussionHub");
+
+//app.Run();
 using Microsoft.AspNetCore.HttpOverrides;
 using CourseWork.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
@@ -9,11 +171,10 @@ using CourseWork.Models;
 using Microsoft.Extensions.Options;
 using CourseWork.Controllers;
 using Microsoft.AspNetCore.Localization;
-using Microsoft.AspNetCore.Mvc.Razor; 
+using Microsoft.AspNetCore.Mvc.Razor;
 using System.Globalization;
-using Microsoft.AspNetCore.HttpOverrides;
-var builder = WebApplication.CreateBuilder(args);
 
+var builder = WebApplication.CreateBuilder(args);
 
 // ---------------- AUTH ----------------
 builder.Services.AddAuthentication(options =>
@@ -26,18 +187,22 @@ builder.Services.AddAuthentication(options =>
 {
     googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
     googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
-
     googleOptions.CallbackPath = "/signin-google";
-
     googleOptions.SaveTokens = true;
 })
 .AddFacebook(facebookOptions =>
 {
     facebookOptions.AppId = builder.Configuration["Authentication:Facebook:AppId"];
     facebookOptions.AppSecret = builder.Configuration["Authentication:Facebook:AppSecret"];
-    facebookOptions.CallbackPath = "/Registration/ExternalLoginCallback"; 
+    facebookOptions.CallbackPath = "/Registration/ExternalLoginCallback";
 });
 
+// Настройка куки для работы через HTTPS и прокси
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+    options.Cookie.SameSite = SameSiteMode.None;
+});
 
 // ---------------- HTTPS ----------------
 builder.Services.AddHttpsRedirection(options =>
@@ -55,8 +220,8 @@ builder.Services.AddHsts(options =>
 
 // ---------------- MVC + Localizer ----------------
 builder.Services.AddControllersWithViews()
-    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix) // 👈 добавлено
-    .AddDataAnnotationsLocalization() // 👈 добавлено
+    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+    .AddDataAnnotationsLocalization()
     .AddSessionStateTempDataProvider();
 
 // ---------------- EF + Identity ----------------
@@ -84,10 +249,8 @@ builder.Services.AddSession(options =>
     options.IdleTimeout = TimeSpan.FromMinutes(30);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
-    options.Cookie.SecurePolicy = builder.Environment.IsDevelopment()
-        ? CookieSecurePolicy.SameAsRequest
-        : CookieSecurePolicy.Always;
-    options.Cookie.SameSite = SameSiteMode.Lax;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+    options.Cookie.SameSite = SameSiteMode.None;
 });
 
 builder.Services.AddHttpContextAccessor();
@@ -115,15 +278,28 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+// Forwarded headers должны идти как можно раньше
 app.UseForwardedHeaders(new ForwardedHeadersOptions
 {
-    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
+    KnownProxies = { },
+    KnownNetworks = { }
 });
 
-var fwdOptions = app.Services.GetRequiredService<IOptions<ForwardedHeadersOptions>>().Value;
-fwdOptions.KnownNetworks.Clear();
-fwdOptions.KnownProxies.Clear();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
 
+// Локализация
+var locOptions = app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value;
+app.UseRequestLocalization(locOptions);
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseSession();
+
+// ---------------- Ensure Admin ----------------
 using (var scope = app.Services.CreateScope())
 {
     var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
@@ -134,25 +310,7 @@ using (var scope = app.Services.CreateScope())
     await registrationController.EnsureAdminExists();
 }
 
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-// 👇 вставь это ДО UseRouting()
-var locOptions = app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value;
-app.UseRequestLocalization(locOptions);
-
-app.UseRouting();
-
-app.UseForwardedHeaders(new ForwardedHeadersOptions
-{
-    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-});
-
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseSession();
-
+// ---------------- Routes ----------------
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Registration}/{action=Register}/{id?}");
