3bb64e1bcad12682102226fb3a8e412a2686af97
alina <annadura@gmail.com>
2025-11-04 00:06:09 +0300

    Resolve merge

--- CourseWork/CourseWork/obj/Debug/net9.0/CourseWork.AssemblyInfo.cs
+++ CourseWork/CourseWork/obj/Debug/net9.0/CourseWork.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f69e92ac96e5d5a067aafea9762775239c70ee6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9999318c93fe3044316a468b625c7c68639deee")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
