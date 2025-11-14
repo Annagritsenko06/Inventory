19d3ad31baba9d422858dd72a40b05f9bce6330c
alina <annadura@gmail.com>
2025-11-14 22:17:32 +0300

    update

--- CourseWork/CourseWork/obj/Debug/net9.0/CourseWork.AssemblyInfo.cs
+++ CourseWork/CourseWork/obj/Debug/net9.0/CourseWork.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f194cd7dc9a754e358a53e931b19cf0d9d2c60a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb4f0bc5746bc87b4111544905ea70f4fa6080c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
